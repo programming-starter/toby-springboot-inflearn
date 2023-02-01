ccc61f766f2db03aef40cb11edd81eb75f498f92
parker1609 <psbum1609@gmail.com>
2023-02-02 08:42:20 +0900

    init project

---
+++ src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
@@ -0,0 +1,13 @@
+package me.parker.tobyspringbootinflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringbootInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringbootInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package me.parker.tobyspringbootinflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringbootInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
