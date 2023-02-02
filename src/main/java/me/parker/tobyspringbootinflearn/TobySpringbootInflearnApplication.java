ad530349960c77dd167954ef80e2b788347c6838
parker1609 <psbum1609@gmail.com>
2023-02-03 08:23:38 +0900

    서블릿 컨테이너 띄우기

--- src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
+++ src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
@@ -1,9 +1,16 @@
 package me.parker.tobyspringbootinflearn;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class TobySpringbootInflearnApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Containerless Application");
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        // tomcat, netty, ... 여러가지 웹 서버를 가져오려고 추상화핸놓음.
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start(); // 톰캣 서블릿 컨테이너 동작
     }
 
 }
