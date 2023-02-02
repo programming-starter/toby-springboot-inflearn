8c07b04760ae64dc04572dd783cd0dedaaf2417b
parker1609 <psbum1609@gmail.com>
2023-02-03 08:48:11 +0900

    서블릿 등록

--- src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
+++ src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
@@ -4,12 +4,27 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class TobySpringbootInflearnApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // tomcat, netty, ... 여러가지 웹 서버를 가져오려고 추상화핸놓음.
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().print("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start(); // 톰캣 서블릿 컨테이너 동작
     }
 
