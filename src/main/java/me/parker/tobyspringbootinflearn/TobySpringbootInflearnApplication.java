714d0fef46692a6f89046b64e46e96b73845fe45
parker1609 <psbum1609@gmail.com>
2023-02-03 08:16:03 +0900

    Containerless 개발 준비

--- src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
+++ src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
@@ -1,13 +1,9 @@
 package me.parker.tobyspringbootinflearn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TobySpringbootInflearnApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobySpringbootInflearnApplication.class, args);
+        System.out.println("Hello Containerless Application");
     }
 
 }
