6e0ff51958fc4751147f32f152f58c1c6b517646
parker1609 <psbum1609@gmail.com>
2023-02-05 10:40:13 +0900

    프론트 컨트롤러로 전환

--- src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
+++ src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -19,18 +20,25 @@ public class TobySpringbootInflearnApplication {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // tomcat, netty, ... 여러가지 웹 서버를 가져오려고 추상화핸놓음.
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // request
-                    String name = req.getParameter("name");
+                    // 인증, 보안, 다국어 등 공통 기능
+                    if (req.getRequestURI().equals("/hello")
+                            && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
 
-                    // response
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().print("Hello " + name);
+                        // response
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().print("Hello " + name);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
         webServer.start(); // 톰캣 서블릿 컨테이너 동작
     }
