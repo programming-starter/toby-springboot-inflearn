ee877e7f2e2420c78b886d726adf197fc6a0fe58
parker1609 <psbum1609@gmail.com>
2023-02-07 08:25:14 +0900

    의존 오브젝트 추가

--- src/main/java/me/parker/tobyspringbootinflearn/HelloController.java
+++ src/main/java/me/parker/tobyspringbootinflearn/HelloController.java
@@ -1,8 +1,12 @@
 package me.parker.tobyspringbootinflearn;
 
+import java.util.Objects;
+
 public class HelloController {
 
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/me/parker/tobyspringbootinflearn/SimpleHelloService.java
@@ -0,0 +1,8 @@
+package me.parker.tobyspringbootinflearn;
+
+public class SimpleHelloService {
+
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
