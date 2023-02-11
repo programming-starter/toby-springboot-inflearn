d7786ae639c85e548f275f4dc58752cab3727048
parker1609 <psbum1609@gmail.com>
2023-02-11 09:26:53 +0900

    스프링 컨테이너로 통합

--- src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
+++ src/main/java/me/parker/tobyspringbootinflearn/TobySpringbootInflearnApplication.java
@@ -9,19 +9,25 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class TobySpringbootInflearnApplication {
 
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                // 지우면 안됨, 상위 클래스에서의 onRefresh() 메서드도 어떠한 동작을 하므로...
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                // tomcat, netty, ... 여러가지 웹 서버를 가져오려고 추상화핸놓음.
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start(); // 톰캣 서블릿 컨테이너 동작
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        // tomcat, netty, ... 여러가지 웹 서버를 가져오려고 추상화핸놓음.
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-        });
-        webServer.start(); // 톰캣 서블릿 컨테이너 동작
     }
 
 }
