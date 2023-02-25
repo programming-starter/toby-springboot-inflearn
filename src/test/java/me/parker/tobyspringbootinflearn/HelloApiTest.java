c8576aa3ac1f3e05f2e1a264188039e84723cfa6
parker1609 <psbum1609@gmail.com>
2023-02-25 10:49:54 +0900

    DI와 테스트

--- src/test/java/me/parker/tobyspringbootinflearn/HelloApiTest.java
+++ src/test/java/me/parker/tobyspringbootinflearn/HelloApiTest.java
@@ -25,4 +25,16 @@ public class HelloApiTest {
         assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         assertThat(response.getBody()).isEqualTo("Hello Spring");
     }
+
+    @Test
+    void failsHelloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> response = rest.getForEntity(
+                "http://localhost:8080/hello?name",
+                String.class
+        );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
