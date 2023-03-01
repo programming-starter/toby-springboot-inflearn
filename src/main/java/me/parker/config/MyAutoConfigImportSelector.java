9e5db21f063390a9216c3d3d05b09c9abade6fba
parker1609 <psbum1609@gmail.com>
2023-03-02 08:11:25 +0900

    자동 구성 정보 파일 분리

--- src/main/java/me/parker/config/MyAutoConfigImportSelector.java
+++ src/main/java/me/parker/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,23 @@
 package me.parker.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "me.parker.config.autoconfig.DispatcherServletConfig",
-                "me.parker.config.autoconfig.TomcatWebServerConfig"
-        };
+        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
