c8af8f51eaf73653cfd4885aaa402cb758bf8b61
parker1609 <psbum1609@gmail.com>
2023-03-03 08:02:05 +0900

    자동 구성 애노테이션 적용

--- src/main/java/me/parker/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/me/parker/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package me.parker.config.autoconfig;
 
+import me.parker.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     @Bean
--- src/main/java/me/parker/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/me/parker/config/autoconfig/TomcatWebServerConfig.java
@@ -1,11 +1,11 @@
 package me.parker.config.autoconfig;
 
+import me.parker.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
 
     @Bean
