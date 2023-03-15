f78f93eb68474a099fbaf77a182de5a91387a1b1
parker1609 <psbum1609@gmail.com>
2023-03-16 08:15:43 +0900

    @Value 와 PropertySourcesPlaceholderConfigurer

---
+++ src/main/java/me/parker/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,14 @@
+package me.parker.config.autoconfig;
+
+import me.parker.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
