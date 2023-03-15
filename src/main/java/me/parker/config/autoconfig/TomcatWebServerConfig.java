44e016c1992bb1c8e54caf62413ef70895a423fa
parker1609 <psbum1609@gmail.com>
2023-03-16 08:29:33 +0900

    프로퍼티 클래스의 분리

--- src/main/java/me/parker/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/me/parker/config/autoconfig/TomcatWebServerConfig.java
@@ -2,7 +2,6 @@ package me.parker.config.autoconfig;
 
 import me.parker.config.ConditionalMyOnClass;
 import me.parker.config.MyAutoConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -12,14 +11,13 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-    @Value("${contextPath}")
-    String contextPath;
-
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(this.contextPath);
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
 
         return factory;
     }
--- src/test/java/me/parker/tobyspringbootinflearn/HelloApiTest.java
+++ src/test/java/me/parker/tobyspringbootinflearn/HelloApiTest.java
@@ -16,7 +16,7 @@ public class HelloApiTest {
         TestRestTemplate rest = new TestRestTemplate();
 
         ResponseEntity<String> response = rest.getForEntity(
-                "http://localhost:8080/app/hello?name={name}",
+                "http://localhost:9090/app/hello?name={name}",
                 String.class,
                 "Spring"
         );
@@ -31,7 +31,7 @@ public class HelloApiTest {
         TestRestTemplate rest = new TestRestTemplate();
 
         ResponseEntity<String> response = rest.getForEntity(
-                "http://localhost:8080/app/hello?name",
+                "http://localhost:9090/app/hello?name",
                 String.class
         );
 
