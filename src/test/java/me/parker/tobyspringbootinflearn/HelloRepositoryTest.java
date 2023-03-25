23fa9a2af93f8c3ffc06fb7f4ac7af207c9f521f
parker1609 <psbum1609@gmail.com>
2023-03-25 09:48:36 +0900

    Hello 리포지토리

---
+++ src/test/java/me/parker/tobyspringbootinflearn/HelloRepositoryTest.java
@@ -0,0 +1,39 @@
+package me.parker.tobyspringbootinflearn;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@HelloBootTest
+class HelloRepositoryTest {
+
+    @Autowired
+    HelloRepository helloRepository;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void findHelloFailed() {
+        assertThat(helloRepository.findHello("Toby")).isNull();
+    }
+
+    @Test
+    void increaseCount() {
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(0);
+
+        helloRepository.increaseCount("Toby");
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(1);
+
+        helloRepository.increaseCount("Toby");
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(2);
+    }
+}
